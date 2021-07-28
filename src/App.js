054e2e29bc20f4a7b4f8029a4ab9d62b5185c2a7
BokovRoman <r.bokov1991@gmail.com>
2021-07-28 22:24:46 +0300

    completed the task

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import './App.css';
 import { Route, Switch } from "react-router-dom";
-import HomePage from './views/HomePage';
-import MoviesPage from './views/MoviesPage';
-import MovieDetailsPage from './views/MovieDetailsPage';
+import { Suspense, lazy } from "react";
 
+import routes from "./routes";
+import AppBar from "./components/AppBar";
+import Loader from "./components/Loader";
+
+const HomePage = lazy(() =>
+  import("./views/HomePage" /* webpackChunkName: "home-page" */)
+);
+const MoviesPage = lazy(() =>
+  import("./views/MoviesPage" /* webpackChunkName: "movies-page" */)
+);
+const MovieDetailsPage = lazy(() =>
+  import("./views/MovieDetailsPage" /* webpackChunkName: "movie-details" */)
+);
 
 const App = () => (
   <>
-
-    {/* <Route exact path={routes.home} component={HomePage} />
+    <AppBar />
+    <Suspense fallback={<Loader />}>
+      <Switch>
+        <Route exact path={routes.home} component={HomePage} />
         <Route path={routes.movieDetails} component={MovieDetailsPage} />
-        <Route path={routes.movies} component={MoviesPage} /> */}
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/movies" component={MoviesPage} />
-      <Route path="/moviesDetails" component={MovieDetailsPage} />
-    </Switch>
+        <Route path={routes.movies} component={MoviesPage} />
+
+        <Route component={HomePage} />
+      </Switch>
+    </Suspense>
   </>
 );
-
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+
+const routes = {
+    home: "/",
+    movies: "/movies",
+    movieDetails: "/movies/:movieId",
+    movieCast: "/movies/:movieId/cast",
+    movieReviews: "/movies/:movieId/reviews ",
+};
+
+export default routes;
